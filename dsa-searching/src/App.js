0d653219e76a30c6fa3662bc92aacfa62de10a57
Jvymvn <Jordon.carter@yahoo.com>
2019-09-06 00:03:01 -0400

    Update

--- dsa-searching/src/App.js
+++ dsa-searching/src/App.js
@@ -3,6 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 import Search from './Search';
 
+function recursiveSearch(data, searchValue, start = 0, end = data.length - 1, count = 0) {
+  // let count = this.state.count;
+  // console.log(count)
+  if (start > end) {
+    return { found: false, count }
+  }
+
+  const middle = Math.floor((start + end) / 2);
+  const item = data[middle]
+
+  console.log(start, end)
+
+  if (item == searchValue) {
+    console.log(middle);
+    console.log('Count: ', count)
+    return { middle, count };
+  }
+  if (item > searchValue) {
+    return recursiveSearch(data, searchValue, start, middle - 1, count + 1);
+  }
+  if (item < searchValue) {
+    return recursiveSearch(data, searchValue, middle + 1, end, count + 1);
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -29,16 +54,81 @@ class App extends React.Component {
     return;
   }
 
-  // binarySearch(){
+  // binarySearch(e) {
+  //   e.preventDefault();
   //   let data = this.state.dataset;
+  //   let searchValue = this.state.searchValue;
+
+  //   const num = recursiveSearch(data, searchValue);
+  //   console.log(num)
+  //   if (num.result) {
+  //     return this.setState({ result: `Found Number ${num.searchValue}`, count: num.count })
+  //   } else {
+  //     return this.setState({ result: 'Not Found', count: num.count })
+  //   }
   // }
+
+  binarySearch(e) {
+    let data = this.state.dataset;
+    let targetValue = this.state.searchValue;
+    const output = recursiveSearch(data, targetValue)
+    //Represents index in array
+    console.log(output)
+
+    if (data[output.middle] == targetValue) {
+      return this.setState({ result: `Found Number ${targetValue}`, count: output.count })//, count: this.state.count 
+    } else {
+      return this.setState({ result: 'Not Found' })//, count: this.state.count 
+    }
+
+
+
+
+
+    // const item = data[index];
+    // console.log(item)
+    // if (targetValue == item) {
+    //   this.setState({ result: `Number Found ${item}`, count: count })
+    // } else if (item < targetValue) {
+    //   return this.binarySearch()
+    // }
+
+
+    // if (item == targetValue) {
+    //   console.log(count);
+    //   this.setState({ count: count });
+    // } else if (item < targetValue) {
+    //   return this.binarySearch(data, targetValue, index + 1, end, count);
+    // } else if (item > targetValue) {
+    //   return this.binarySearch(data, targetValue, start, index - 1, count);
+    // } else {
+    //   console.log('else');
+    //   this.setState({
+    //     result: `item not found after ${count} attempts`
+    //   });
+  }
+
+  // binarySearchAlgo(arr, targetValue, start = 0, end = arr.length - 1) {
+  //   const middle = Math.floor((start + end) / 2);
+  //   let searchValue = this.state.searchValue
+  //   count++;
+
+  //   const item = arr[middle]
+  //   if (searchValue === item) {
+  //     this.setState({ result: `Number Found ${item}`, count: count })
+  //   }
+
+  // }
+
+
 
   render() {
     return (
       <div className="App">
-        <label>Search for number in dataset</label>
+        <label>Linear search / Binary Search</label>
         <input type='text' value={this.state.searchValue} onChange={(event) => this.setState({ searchValue: event.target.value })} />
         <button type='submit' onClick={(e) => this.linearSearch(e)}>Linear Search</button>
+        <button type='submit' onClick={(e) => this.binarySearch(e)}>Binary Search</button>
         <h2>{this.state.result}, found in {this.state.count} number of tries.</h2>
       </div>
     );
